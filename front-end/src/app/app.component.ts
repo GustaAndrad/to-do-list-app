c390da19f711a706be8e3adc369ded7036dd697b
Gustavo Andrade <gusta.andradev@gmail.com>
2022-05-12 18:41:34 -0300

    implementando formulario para enviar json para api

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  mensagem : string = "hello world"
+  
+  form: FormGroup = new FormGroup({
+    description : new FormControl('')
+  })
+
+  submit(){
+    console.log(this.form.value)
+  }
 }
