5e00d858b8d6fceb89d11c0fd4085f8c39f809b1
Gustavo Andrade <gusta.andradev@gmail.com>
2022-05-13 12:46:53 -0300

    Salvando registro na API

--- api/src/main/java/com/GustaAndrad/toDoListApp/rest/ToDoListController.java
+++ api/src/main/java/com/GustaAndrad/toDoListApp/rest/ToDoListController.java
@@ -2,6 +2,7 @@ package com.GustaAndrad.toDoListApp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.GustaAndrad.toDoListApp.repository.RepositoryJPA;
 
 @RestController
 @RequestMapping("/api/todolist")
+@CrossOrigin("http://localhost:4200")
 public class ToDoListController {
 
 	@Autowired
