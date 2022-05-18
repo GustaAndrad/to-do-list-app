66a0cf16f3ef53ab1358ed59cb1029e834cbb903
Gustavo Andrade <gusta.andradev@gmail.com>
2022-05-17 22:20:06 -0300

    metodo delete

--- api/src/main/java/com/GustaAndrad/toDoListApp/rest/ToDoListController.java
+++ api/src/main/java/com/GustaAndrad/toDoListApp/rest/ToDoListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,10 @@ public class ToDoListController {
 	public Todolist getById(@PathVariable Long id) {
 		return repository.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("{id}")
+	public void deleteById(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
